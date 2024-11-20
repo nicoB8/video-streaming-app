25c8e7cb50cf3219116a765b01a2a7479f120db7
Nicolas Bonora <nicobonora08@gmail.com>
2024-11-20 04:23:06 -0300

    Base App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit } from '@angular/core';
+import Hls from 'hls.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'video-streaming-app';
+export class AppComponent implements AfterViewInit {
+  streamUrl: string = 'https://22c46707142c.us-east-1.playback.live-video.net/api/video/v1/us-east-1.466068279757.channel.ZIdp2OwU4fyR.m3u8';
+
+  ngAfterViewInit(): void {
+    const video = document.getElementById('videoPlayer') as HTMLVideoElement;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(this.streamUrl);
+      hls.attachMedia(video);
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = this.streamUrl;
+    }
+  }
 }
